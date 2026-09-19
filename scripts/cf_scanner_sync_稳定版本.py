0d7b17230fd12ed778c4901786a4bfefd5deb85a
youngjoffjun <139073762+youngjoffjun@users.noreply.github.com>
2026-09-19 08:25:05 +0800

    Update cf_scanner_sync_稳定版本.py

--- scripts/cf_scanner_sync_稳定版本.py
+++ scripts/cf_scanner_sync_稳定版本.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta, timezone
 # 支持多个地区，用逗号隔开，例如 "SJC,LAX,HKG,FRA,NRT"
 # 💡 新手不知道有什么地区？可以直接填 "ALL"，系统会全区盲扫并自动创建所有能扫到的地区子域名！
 # ==========================================
-DEFAULT_REGIONS = "SJC"
+DEFAULT_REGIONS = "SJC,LAX,HKG,FRA,NRT"
 
 # 🌐 主域名终极大汇总同步开关
 # 设置为 "YES": 开启！将所有扫到的极品节点汇总推送到你的主域名（全球负载均衡）
